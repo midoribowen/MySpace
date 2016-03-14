91629af3bc0ca6d9f8f980175eb696f958b348ef
Illia Kuchko and Midori Bowen <midori.p.bowen@gmail.com>
2016-03-14 15:35:28 -0700

    add intent to navigate to CreateSpace activity.

---
+++ app/src/main/java/com/epicodus/myspace/CreateSpace.java
@@ -0,0 +1,13 @@
+package com.epicodus.myspace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateSpace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_space);
+    }
+}
--- app/src/main/java/com/epicodus/myspace/MainActivity.java
+++ app/src/main/java/com/epicodus/myspace/MainActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.myspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bSignUp = (Button) findViewById(R.id.buttonSignUp);
+        bSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateSpace.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
