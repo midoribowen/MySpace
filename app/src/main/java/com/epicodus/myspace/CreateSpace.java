69d89bc4e7cc1f1d0c2f1972dba6264680dbf5a7
Illia Kuchko and Midori Bowen <ikuchko@gmail.com>
2016-03-14 16:50:45 -0700

    add signup input form.

--- app/src/main/java/com/epicodus/myspace/CreateSpace.java
+++ app/src/main/java/com/epicodus/myspace/CreateSpace.java
@@ -2,6 +2,8 @@ package com.epicodus.myspace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateSpace extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CreateSpace extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_space);
+
+        Spinner countrySpinner = (Spinner) findViewById(R.id.countrySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countries_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        countrySpinner.setAdapter(adapter);
     }
 }
